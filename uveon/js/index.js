33981503b22fa03e0386f61d533b38ca0aaa2806
RomanMereneanu <asasin84@mail.ru>
2021-02-20 16:41:55 +0200

    add

--- uveon/js/index.js
+++ uveon/js/index.js
@@ -1,4 +1,19 @@
 "use strict";
 (function () {
+    var opacity_all = document.querySelectorAll(".opacity_all");
+    window.addEventListener('scroll', function () {
+
+        opacity_all.forEach(function (element) {
+            let h_element = element.offsetHeight;
+            let pageHeight = element.getBoundingClientRect().top;
+            pageHeight = pageHeight - h_element;
+            if (pageHeight < 0) {
+                element.style.opacity = 1;
+                element.style.transition = 'opacity 0.8s ease';
+            }
+        })
+
+    })
+
 
 })()
